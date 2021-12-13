7c8f987267e0cad3f4b514533322b4a06e5bbb57
bintangjtobing <hello@bintangtobing.com>
2021-12-13 16:51:51 +0700

    update header image

--- resources/views/homepage/contact.blade.php
+++ resources/views/homepage/contact.blade.php
@@ -3,9 +3,8 @@
 @section('activecontact','current')
 @section('content')
 <div class="section parallax dark bottommargin-lg" style="background-image:
-    url('https://www.vtldesign.hu/wp-content/uploads/2014/07/web-development-background.jpg');
-    background-size: cover;" data-bottom-top="background-position:0px 0px; margin-top:0px;"
-    data-top-bottom="background-position:0px -500px;">
+    url('https://res.cloudinary.com/https-www-infinitysolutions-co-id/image/upload/c_scale,w_1920/v1639388484/customer/multimaschemindo/lainlain/header-contact-us.jpg');
+    background-size: cover;" data-bottom-top="background-position:0px 0px; margin-top:0px;">
     <div class="container clearfix">
         <div class="row align-content-between">
             <div class="col-lg-12 text-center" data-animate="bounceIn">
--- resources/views/homepage/partners.blade.php
+++ resources/views/homepage/partners.blade.php
@@ -3,9 +3,8 @@
 @section('activesuppliers','current')
 @section('content')
 <div class="section parallax dark bottommargin-lg" style="background-image:
-    url('https://lh3.googleusercontent.com/XpOgKIwa5xclaCZ5bjqIruhKvBUQE_to9uVY9o5A2mRHFw00mvYl287VLCZ4Vus=s2600');
-    background-size: cover;" data-bottom-top="background-position:0px 0px; margin-top:0px;"
-    data-top-bottom="background-position:0px -500px;">
+    url('https://res.cloudinary.com/https-www-infinitysolutions-co-id/image/upload/c_scale,w_1920/v1639388484/customer/multimaschemindo/lainlain/header-key-partners.jpg');
+    background-size: cover;" data-bottom-top="background-position:0px 0px; margin-top:0px;">
     <div class="container clearfix">
         <div class="row align-content-between">
             <div class="col-lg-12 text-center" data-animate="bounceIn">
